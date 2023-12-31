149a7b395de40607e1d089dc17e81cdd0a62caa5
Ayushi12-spec <121248385+Ayushi12-spec@users.noreply.github.com>
2023-12-31 19:02:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+function Formdata(event){
+
+    event.preventDefault()
+
+    let name=document.getElementById('name').value
+    let number=document.getElementById('contact').value
+    let email=document.getElementById('email').value
+
+    let obj={
+       
+        name:name,
+        number: number,
+        email:email
+          
+    }
+
+    userOncurdcurd(obj)
+    
+
+}
+
+
+async function userOncurdcurd(obj){
+
+    try {
+        let result=await axios.post("https://crudcrud.com/api/703ce4f9fdfd4321a3737c7911622e94/studentdata", obj)
+        useronscreen(result.data)
+    } catch (error) {
+
+        console.log(error)
+        
+    }
+
+
+
+}
+
+function useronscreen(obj){
+
+    var a=document.createElement('li')
+    a.textContent=obj.name+" "+ obj.email+" "+obj.number
+    var b= document.getElementById("list")
+    b.appendChild(a)
+    var c= document.createElement("input")
+    c.value="delete"
+    c.type="button"
+    a.appendChild(c)
+    
+
+}
